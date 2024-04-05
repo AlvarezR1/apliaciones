bcd3a1278e6fe39d16a36cfafeb090fbb431d5b9
Alvarez19 <ALVAREZ200319@GMAIL.COM>
2024-04-05 16:51:11 -0500

    vista de admin
    no me funes Liz, aun quiero vivir

---
+++ admin.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Vista_Admin</title>
+</head>
+<body>
+    <!-- Sidebar -->
+<div class="w3-sidebar w3-light-grey w3-bar-block" style="width:25%">
+  <h3 class="w3-bar-item">Randy</h3>
+  <a href="#" class="w3-bar-item w3-button">Inicio</a>
+  <a href="#" class="w3-bar-item w3-button">Usuarios</a>
+  <a href="#" class="w3-bar-item w3-button">Tareas</a>
+  <a href="#" class="w3-bar-item w3-button">Pendientes</a>
+  <a href="#" class="w3-bar-item w3-button">Atrasados</a>
+  <a href="#" class="w3-bar-item w3-button">Completados</a>
+  <a href="#" class="w3-bar-item w3-button">Salir</a>
+</div>
+
+<!-- Page Content -->
+<div style="margin-left:25%">
+
+<div class="w3-container w3-teal">
+  <h1>Gestor de Administrador</h1>
+</div>
+
+<div class="w3-container">
+  <h2>Admin</h2>
+  <p>*inserta texto*</p>
+
+  <table class="w3-table-all w3-card-4">
+    <tr>
+      <th>Nombre</th>
+      <th>Correo</th>
+      <th>Tipo</th>
+    </tr>
+    <tr>
+      <td>Juan</td>
+      <td>toto@gmail.com</td>
+      <td>Usuario</td>
+    </tr>
+    <tr>
+      <td>Raul</td>
+      <td>Homo@sexual.com</td>
+      <td>Admin</td>
+    </tr>
+    <tr>
+      <td>Liz</td>
+      <td>papito@hotmail.com</td>
+      <td>Usuario</td>
+    </tr>
+  </table>
+</div>
+
+</body>
+</html>
