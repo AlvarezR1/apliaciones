1de92b3481b1cfb252d39bbd34c3b315c2c9fbf8
David Isai <91503790+Davidav18@users.noreply.github.com>
2024-04-05 19:35:34 -0600

    nav-bar

--- pages/adminpage/adminpage.php
+++ pages/adminpage/adminpage.php
@@ -1,11 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>adminpage</title>
+    <title>user page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
+
 <body>
-    <h1>admin</h1>
+    <h1>useer</h1>
+    <div class="container-fluid">
+        <div class="row flex-nowrap">
+
+            <div class="bg-dark col-auto col-md-2 min-vh-100">
+                <div class="bg-dark">
+                    <a class="d-flex text-decoration-none align items - center "">
+                      <span class="fs-4 d-none d-sm-inline ">sidemenu</span>
+
+
+
+                    </a>
+                    <ul class="nav nav-pills flex column">
+                        <li class="nav-item">
+                            <a href="#" class=""></a>
+                        </li>
+
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
