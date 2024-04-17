34da710722314a6a3210e2a3b389a259515e792d
David Isai <91503790+Davidav18@users.noreply.github.com>
2024-04-16 20:25:23 -0600

    margin-left
    margin.left

--- pages/adminpage/tareas.php
+++ pages/adminpage/tareas.php
@@ -60,7 +60,7 @@
     $query = "SELECT * FROM tareas";
     $result = $connect->query($query);
     ?>
-    <div style="margin-left:25%">
+    <div style="margin-left:20%">
         <div class="w3-container w3-teal">
             <h1>USUARIOS</h1>
         </div>
